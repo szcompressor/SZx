a753e868b04ea5293d676858955355b82ffbba9c
Xiaodong <xyu@thetalogin4.tmi.alcf.anl.gov>
2021-06-19 22:15:40 +0000

    Cleanup GPU decompression code.

--- sz/include/cuSZx_decompress_float.h
+++ sz/include/cuSZx_decompress_float.h
@@ -7,6 +7,6 @@
 #include <helper_cuda.h>  // helper function CUDA error checking and initialization
 #include <helper_functions.h>  // helper for shared functions common to CUDA Samples
 
-__global__ void decompress_float(unsigned char *data, int bs, size_t nc, size_t mSize, int *test); 
+__global__ void decompress_float(unsigned char *data, int bs, size_t nc, size_t mSize); 
 
 #endif /* ----- #ifndef CUSZX_DECOMPRESS_FLOAT_H  ----- */
