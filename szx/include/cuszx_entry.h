e33d1844111df7b5b8257b5772d64c476a5cf072
Xiaodong Yu <xyu@gpulogin2.lcrc.anl.gov>
2022-02-16 17:37:42 -0600

    Add CUDA files.

---
+++ szx/include/cuszx_entry.h
@@ -0,0 +1,13 @@
+#ifndef CUSZX_ENTRY_H
+#define CUSZX_ENTRY_H
+
+#include <stdio.h>
+#include "cuszx_float.h"
+#include "cuszxd_float.h"
+
+#define GPU
+
+unsigned char* cuSZx_fast_compress_args_unpredictable_blocked_float(float *oriData, size_t *outSize, float absErrBound, size_t nbEle, int blockSize);
+void cuSZx_fast_decompress_args_unpredictable_blocked_float(float** newData, size_t nbEle, unsigned char* cmpBytes);
+
+#endif /* ----- #ifndef CUSZX_ENTRY_H  ----- */
---
+++ szx/include/cuszx_float.h
@@ -0,0 +1,19 @@
+#ifndef CUSZX_FLOAT_H
+#define CUSZX_FLOAT_H
+
+#include <cuda_runtime.h>
+
+// Utilities and system includes
+#include <helper_cuda.h>  // helper function CUDA error checking and initialization
+#include <helper_functions.h>  // helper for shared functions common to CUDA Samples
+
+#define FULL_MASK 0xffffffff
+
+__device__
+void reduction(double sum1, double sum2,
+        double minDiff, double maxDiff, double sumDiff, double sumOfDiffSquare, 
+        double minErr, double maxErr, double sumErr, double sumErrSqr);
+
+__global__ void compress_float(float *oriData, unsigned char *meta, short *offsets, unsigned char *midBytes, float absErrBound, int bs, size_t nb, size_t mSize); 
+
+#endif /* ----- #ifndef CUSZX_COMPRESS_FLOAT_H  ----- */
