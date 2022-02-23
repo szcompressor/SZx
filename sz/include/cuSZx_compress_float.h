029889ee205ddd99d325e6f50bc4be0c48832ac2
Xiaodong Yu <xiyu@login1.summit.olcf.ornl.gov>
2022-02-23 18:41:09 -0500

    Update header files.

--- sz/include/cuSZx_compress_float.h
+++ sz/include/cuSZx_compress_float.h
@@ -1,11 +1,11 @@
 #ifndef CUSZX_COMPRESS_FLOAT_H
 #define CUSZX_COMPRESS_FLOAT_H
 
-#include <cuda_runtime.h>
+#include <hip/hip_runtime.h>
 
 // Utilities and system includes
-#include <helper_cuda.h>  // helper function CUDA error checking and initialization
-#include <helper_functions.h>  // helper for shared functions common to CUDA Samples
+//#include <helper_cuda.h>  // helper function CUDA error checking and initialization
+//#include <helper_functions.h>  // helper for shared functions common to CUDA Samples
 
 #define FULL_MASK 0xffffffff
 
