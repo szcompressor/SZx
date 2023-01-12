17298e39fe10199a9361d2cbf302ca966c9ed0c9
JiajunHuang1999 <jiajunhuang19990916@gmail.com>
2023-01-11 20:58:26 -0800

    add SZ_fast_compress_args2

--- szx/include/szx.h
+++ szx/include/szx.h
@@ -88,6 +88,8 @@ size_t computeDataLength(size_t r5, size_t r4, size_t r3, size_t r2, size_t r1);
 int filterDimension(size_t r5, size_t r4, size_t r3, size_t r2, size_t r1, size_t* correctedDimension);
 unsigned char* SZ_fast_compress_args(int fastMode, int dataType, void *data, size_t *outSize, int errBoundMode, float absErrBound,
 float relBoundRatio, float compressionRatio, float tolerance, size_t r5, size_t r4, size_t r3, size_t r2, size_t r1);
+unsigned char* SZ_fast_compress_args2(int fastMode, int dataType, void *data, size_t *outSize, unsigned char* outputBytes, int errBoundMode, float absErrBound,
+float relBoundRatio, float compressionRatio, float tolerance, size_t r5, size_t r4, size_t r3, size_t r2, size_t r1);
 void* SZ_fast_decompress_pred(int dataType, float* preData, unsigned char *curBytes, size_t byteLength, size_t r5, size_t r4, size_t r3, size_t r2, size_t r1);
 void* SZ_fast_decompress(int fastMode, int dataType, unsigned char *bytes, size_t byteLength, size_t r5, size_t r4, size_t r3, size_t r2, size_t r1);
 
