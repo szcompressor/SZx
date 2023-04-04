181613a8e3f84adbecf42b68078172bfc9e3ac7a
Milan Shah <mkshah5@ncsu.edu>
2023-04-04 16:26:31 -0400

    Updated artifacts compress

--- szx/src/cuszx_entry.cu
+++ szx/src/cuszx_entry.cu
@@ -4,6 +4,8 @@
 #include "szx_TypeManager.h"
 #include "timingGPU.h"
 
+#define DO_ARTIFACT_ANALYSIS
+
 TimingGPU timer_GPU;
 void bin(unsigned n)
 {
@@ -64,6 +66,170 @@ int _post_proc(float *oriData, unsigned char *meta, short *offsets, unsigned cha
     return out_size;
 }
 
+__device__ void fits_linear(float y1, float y2, float* block_data, int blockSize, float absErrBound, float median){
+    int fits_bound = 1;
+    float mse_l = 0.0;
+    float mse_m = 0.0;
+    for (float x = 0.0, int i=0; i < blockSize; i++, x+=1.0)
+    {
+        float y = y1+((y2-y1)/((float)blockSize))*x;
+        float diff_l = y - block_data[i];
+        float diff_m = median - block_data[i];
+        if (fabs(diff_l) >absErrBound)
+        {
+            fits_bound = 0;
+        }
+        mse_l+= diff_l*diff_l;
+        mse_m+= diff_m*diff_m;
+    }
+
+    mse_l = mse_l/(float)blockSize;
+    mse_m = mse_m/(float)blockSize;
+}
+
+__global__ void artifact_analysis(float *oriData, int blockSize, float absErrBound, size_t nbEle, unsigned char *meta, int xdim, int ydim, int zdim, unsigned char *artifact_type){
+    size_t ncBytes = blockSize/4;
+    size_t mSize = sizeof(float)+1+ncBytes;
+    size_t nbBlocks = nbEle/blockSize;
+
+    int blocks_per_z = xdim*ydim/blockSize;
+
+    for (int d = blockIdx.x; d < zdim; d+=gridDim.x) // Each block handles 2D grid
+    {
+        int startblock = d*blocks_per_z;
+        int blocks_per_y = xdim/blockSize;
+        for (int r = threadIdx.x; r < ydim; r+=blockDim.x) // Each thread handles one row of 2D grid
+        {
+            int rowstartblock = startblock + r*blocks_per_y;
+            float prevblock_interpolate, nextblock_interpolate, median;
+            memcpy(&prevblock_interpolate, meta+(nbBlocks+rowstartblock*mSize), sizeof(float));
+            for (int c = rowstartblock; c < rowstartblock+blocks_per_y; c++) // For each chunk in row...
+            {
+                if (c > rowstartblock && c < (rowstartblock+blocks_per_y)-1)
+                {
+                    
+                    if (meta[c-1] == 0 && meta[c] == 0 && meta[c+1] == 0) //three constant blocks
+                    {
+                        memcpy(&nextblock_interpolate, meta+(nbBlocks+(c+1)*mSize), sizeof(float));
+                        memcpy(&median, meta+(nbBlocks+(c)*mSize), sizeof(float));
+                        int fits_bound = 1;
+                        float mse_l = 0.0;
+                        float mse_m = 0.0;
+                        float y1 = prevblock_interpolate;
+                        float y2 = nextblock_interpolate;
+                        float last_point = 0.0;
+                        for (float x = 0.0, int i=0; i < blockSize; i++, x+=1.0)
+                        {
+                            float y = y1+((y2-y1)/((float)blockSize))*x;
+                            float diff_l = y - block_data[i];
+                            float diff_m = median - block_data[i];
+                            if (fabs(diff_l) >absErrBound)
+                            {
+                                fits_bound = 0;
+                            }
+                            if (i == blockSize-1)
+                            {
+                                last_point = y;
+                            }
+                            
+                            mse_l+= diff_l*diff_l;
+                            mse_m+= diff_m*diff_m;
+                        }
+
+                        mse_l = mse_l/(float)blockSize;
+                        mse_m = mse_m/(float)blockSize;
+                        if (mse_l < mse_m && fits_bound==1)
+                        {
+                            artifact_type[c] = 1;
+                        }else{
+                            artifact_type[c] = 0;
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+}
+
+// Note that median values must be of length nbBlocks not number of nonconstant blocks
+__global__ void artifact_reconstruct(float *newData, float *medianvalues, unsigned char* states, int blockSize, float absErrBound, size_t nbEle, unsigned char *meta, int xdim, int ydim, int zdim, unsigned char *artifact_type){
+    size_t ncBytes = blockSize/4;
+    size_t mSize = sizeof(float)+1+ncBytes;
+    size_t nbBlocks = nbEle/blockSize;
+
+    int blocks_per_z = xdim*ydim/blockSize;
+
+    for (int d = blockIdx.x; d < zdim; d+=gridDim.x) // Each block handles 2D grid
+    {
+        int startblock = d*blocks_per_z;
+        int blocks_per_y = xdim/blockSize;
+        for (int r = threadIdx.x; r < ydim; r+=blockDim.x) // Each thread handles one row of 2D grid
+        {
+            int rowstartblock = startblock + r*blocks_per_y;
+            float prevblock_interpolate, nextblock_interpolate, median;
+            prevblock_interpolate = medianvalues[rowstartblock];
+            for (int c = rowstartblock; c < rowstartblock+blocks_per_y; c++) // For each chunk in row...
+            {
+                if (c > rowstartblock && c < (rowstartblock+blocks_per_y)-1)
+                {
+                    
+                    if (artifact_type[c]==1) //three constant blocks
+                    {
+
+                        nextblock_interpolate = medianvalues[c];
+                        median = medianvalues[c];
+                        int fits_bound = 1;
+                        float mse_l = 0.0;
+                        float mse_m = 0.0;
+                        float y1 = prevblock_interpolate;
+                        float y2 = nextblock_interpolate;
+                        float last_point = 0.0;
+                        for (float x = 0.0, int i=0; i < blockSize; i++, x+=1.0)
+                        {
+                            float y = y1+((y2-y1)/((float)blockSize))*x;
+                            float diff_l = y - block_data[i];
+                            float diff_m = median - block_data[i];
+                            if (fabs(diff_l) >absErrBound)
+                            {
+                                fits_bound = 0;
+                            }
+                            if (i == blockSize-1)
+                            {
+                                last_point = y;
+                            }
+                            
+                            mse_l+= diff_l*diff_l;
+                            mse_m+= diff_m*diff_m;
+                        }
+
+                        mse_l = mse_l/(float)blockSize;
+                        mse_m = mse_m/(float)blockSize;
+                        if (mse_l < mse_m && fits_bound==1)
+                        {
+                            artifact_type[c] = 1;
+                        }else{
+                            artifact_type[c] = 0;
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+}
+
+
 unsigned char* cuSZx_fast_compress_args_unpredictable_blocked_float(float *oriData, size_t *outSize, float absErrBound, size_t nbEle, int blockSize)
 {
 
@@ -108,6 +274,71 @@ unsigned char* cuSZx_fast_compress_args_unpredictable_blocked_float(float *oriDa
     checkCudaErrors(cudaMemcpy(offsets, d_offsets, nbBlocks*sizeof(short), cudaMemcpyDeviceToHost)); 
     checkCudaErrors(cudaMemcpy(midBytes, d_midBytes, mbsz, cudaMemcpyDeviceToHost)); 
 
+    size_t maxPreservedBufferSize = sizeof(float)*nbEle;
+    unsigned char* outBytes = (unsigned char*)malloc(maxPreservedBufferSize);
+    memset(outBytes, 0, maxPreservedBufferSize);
+
+    *outSize = _post_proc(oriData, meta, offsets, midBytes, outBytes, nbEle, blockSize);
+
+    free(meta);
+    free(offsets);
+    free(midBytes);
+    checkCudaErrors(cudaFree(d_meta));
+    checkCudaErrors(cudaFree(d_offsets));
+    checkCudaErrors(cudaFree(d_midBytes));
+    return outBytes;
+}
+
+unsigned char* cuSZx_fast_compress_args_artifact_aware(float *oriData, size_t *outSize, float absErrBound, size_t nbEle, int blockSize, int xdim, int ydim, int zdim)
+{
+
+	float* d_oriData;
+    unsigned char *artifact_type, *d_artifact_type;
+    cudaMalloc((void**)&d_oriData, sizeof(float)*nbEle); 
+    cudaMemcpy(d_oriData, oriData, sizeof(float)*nbEle, cudaMemcpyHostToDevice); 
+
+	size_t nbBlocks = nbEle/blockSize;
+	size_t remainCount = nbEle%blockSize;
+	size_t actualNBBlocks = remainCount==0 ? nbBlocks : nbBlocks+1;
+
+    size_t ncBytes = blockSize/4;
+    //ncBytes = (blockSize+1)%4==0 ? ncBytes : ncBytes+1; //Bytes to store one non-constant block data.
+    size_t mSize = sizeof(float)+1+ncBytes; //Number of bytes for each data block's metadata.
+    size_t msz = (1+mSize) * nbBlocks * sizeof(unsigned char);
+    size_t mbsz = sizeof(float) * nbEle * sizeof(unsigned char);
+
+    unsigned char *meta = (unsigned char*)malloc(msz);
+    short *offsets = (short*)malloc(nbBlocks*sizeof(short));
+    unsigned char *midBytes = (unsigned char*)malloc(mbsz);
+
+	unsigned char* d_meta;
+	unsigned char* d_midBytes;
+	short* d_offsets;
+    checkCudaErrors(cudaMalloc((void**)&d_meta, msz)); 
+    //checkCudaErrors(cudaMemcpy(d_meta, meta, msz, cudaMemcpyHostToDevice)); 
+    checkCudaErrors(cudaMemset(d_meta, 0, msz));
+    checkCudaErrors(cudaMalloc((void**)&d_offsets, nbBlocks*sizeof(short))); 
+    checkCudaErrors(cudaMemset(d_offsets, 0, nbBlocks*sizeof(short)));
+    checkCudaErrors(cudaMalloc((void**)&d_midBytes, mbsz)); 
+    checkCudaErrors(cudaMemset(d_midBytes, 0, mbsz));
+    checkCudaErrors(cudaMalloc(&d_artifact_type, sizeof(char)*nbBlocks));
+
+    timer_GPU.StartCounter();
+    dim3 dimBlock(32, blockSize/32);
+    dim3 dimGrid(65536, 1);
+    const int sMemsize = blockSize * sizeof(float) + dimBlock.y * sizeof(int);
+    compress_float<<<dimGrid, dimBlock, sMemsize>>>(d_oriData, d_meta, d_offsets, d_midBytes, absErrBound, blockSize, nbBlocks, mSize);
+    cudaError_t err = cudaGetLastError();        // Get error code
+    printf("CUDA Error: %s\n", cudaGetErrorString(err));
+    printf("GPU compression timing: %f ms\n", timer_GPU.GetCounter());
+    checkCudaErrors(cudaMemcpy(meta, d_meta, msz, cudaMemcpyDeviceToHost)); 
+    checkCudaErrors(cudaMemcpy(offsets, d_offsets, nbBlocks*sizeof(short), cudaMemcpyDeviceToHost)); 
+    checkCudaErrors(cudaMemcpy(midBytes, d_midBytes, mbsz, cudaMemcpyDeviceToHost)); 
+    
+    artifact_analysis<<<40,256>>>(d_oriData, blockSize, absErrBound, nbEle, d_meta, xdim, ydim, zdim, d_artifact_type);
+
+    cudaDeviceSynchronize();
+    checkCudaErrors(cudaMemcpy(artifact_type, d_artifact_type,sizeof(char)*nbBlocks, cudaMemcpyDeviceToHost));
     size_t maxPreservedBufferSize = sizeof(float)*nbEle;
     unsigned char* outBytes = (unsigned char*)malloc(maxPreservedBufferSize);
     memset(outBytes, 0, maxPreservedBufferSize);
