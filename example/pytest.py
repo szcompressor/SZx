c37fc9f5f21395ac04d3db4947d22f911feb0501
Kai Zhao <zhaokaijia@gmail.com>
2023-07-25 14:17:55 -0500

    update python interface

---
+++ example/pytest.py
@@ -0,0 +1,27 @@
+import numpy as np
+from pathlib import Path
+from pyszx import SZx
+import sys
+
+# prepare your data in numpy array format
+HOME = str(Path.home())
+data = np.random.rand(10000).astype(np.float32)
+
+# init SZ (both SZ2 and SZ3 are supported)
+# Please change the path to the SZ dynamic library file in your system
+lib_extention = {
+    "darwin": "libSZx.dylib",
+    "windows": "SZx.dll",
+}.get(sys.platform, "libSZ3c.so")
+
+szx = SZx("../build/lib/{}".format(lib_extention))
+
+# compress, both input and output data are numpy array
+data_cmpr, cmpr_ratio = szx.compress(data, 1, 0, 1e-2)
+print("compression ratio = {:5G}".format(cmpr_ratio))
+
+# decompress, both input and output data are numpy array
+data_dec = szx.decompress(data_cmpr, data.shape, data.dtype)
+
+# verify
+szx.verify(data, data_dec)
